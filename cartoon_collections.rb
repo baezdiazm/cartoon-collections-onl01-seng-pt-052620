5ce4f053a7addf11a6a1adcc43ffd1cfa6e06793
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-07 21:11:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,9 +18,15 @@ def roll_call_dwarves(names)
     cap
   end
 ######################################################################################################################
-  def long_planeteer_calls# code an argument here
-    # Your code here
+def long_planeteer_calls(array)
+  counter = 0
+  returnvalues = []
+  while counter < array.length
+    returnvalues << yield(array[counter])
+    counter += 1
   end
+  puts returnvalues
+end
 ######################################################################################################################
   def find_the_cheese# code an argument here
     # the array below is here to help
