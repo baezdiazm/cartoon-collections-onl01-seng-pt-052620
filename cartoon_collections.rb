a6386fa147bac5df10d980b89808bfefa34b8686
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-07 20:31:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+counter = 0
+while counter < names.length
+names.each do |name| puts "#{counter+1}. #{name}"
+counter +=1
+end
 end
 
 def summon_captain_planet# code an argument here
