35923259a06e89ec2fc9019339e725ad7dbe6136
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-07 20:31:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,20 @@
 def roll_call_dwarves(names)
-counter = 0
-while counter < names.length
-names.each do |name| puts "#{counter+1}. #{name}"
-counter +=1
-end
-end
+  counter = 0
+  while counter < names.length
+    names.each do |name| puts "#{counter+1}. #{name}"
+      counter +=1
+    end
+  end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+  def summon_captain_planet# code an argument here
+    # Your code here
+  end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+  def long_planeteer_calls# code an argument here
+    # Your code here
+  end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  def find_the_cheese# code an argument here
+    # the array below is here to help
+    cheese_types = ["cheddar", "gouda", "camembert"]
+  end
